aebce951694d15f1d2abcd7a2551dc5504766353
gldickens3 <gordon@mailhub4u.com>
2009-11-22 17:27:57 +0000

    Added Logo To exim4u email admin front page.

--- home/exim4u/public_html/exim4u/index.php
+++ home/exim4u/public_html/exim4u/index.php
@@ -57,5 +57,33 @@
       </form>
     </div>
   </body>
+
+
+<body style="margin: 0px;">
+ <div align="center">
+  <table border="0" cellspacing="0" cellpadding="0">
+   <tr>
+    <td><TABLE cellSpacing=0 cellPadding=0 width=800 align=center>
+<TBODY>
+<TR>
+<TD style="BORDER-RIGHT: black 0px solid; BORDER-TOP: black 0px solid; BORDER-LEFT: black 0px
+ solid; BORDER-BOTTOM: black 0px solid">
+</TD></TR></TBODY></TABLE>
+     <table border="0" cellspacing="0" cellpadding="0" width="446">
+      <tr valign="top" align="left">
+       <td width="357" height="255"><img src="./images/clearpixel.gif" width="357" height="1" border="0" alt=""></td>
+       <td></td>
+      </tr>
+      <tr valign="top" align="left">
+       <td height="52"></td>
+       <td width="88"><img id="Exim4U" height="52" width="88" src="./images/exim4u_52_EX.gif" border="0" alt="Exim4U" title="Exim4U"></td>
+      </tr>
+     </table>
+    </td>
+   </tr>
+  </table>
+ </div>
+</body>
+
 </html>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
