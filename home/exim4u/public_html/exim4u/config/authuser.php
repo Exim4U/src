3df8ef4166e3ab2c07dfd8d9b04e86b1639d705d
Gordon Dickens <gordon@mailhub4u.com>
2009-12-27 14:38:15 +0000

    Merging Changelog into tags/1.2.1 and branches/1.2.

---
+++ home/exim4u/public_html/exim4u/config/authuser.php
@@ -0,0 +1,16 @@
+<?php
+  include_once dirname(__FILE__) . "/httpheaders.php";
+  include_once dirname(__FILE__) . "/variables.php";
+  $query = "SELECT user_id,localpart,crypt,domain_id FROM users WHERE localpart='".$_SESSION['localpart']."'
+  		AND domain_id='".$_SESSION['domain_id']."';";
+  $result = $db->query($query);
+  $row = $result->fetchRow();
+
+  // If the localpart isn't in the cookie, of the database
+  // password doesn't match the cookie password, reject the
+  // user to the login screen
+  if ($row['localpart'] != $_SESSION['localpart']) { header ("Location: index.php?login=failed"); };
+  if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed"); };
+  if ($row['user_id'] != $_SESSION['user_id']) {header ("Location: index.php?login=failed"); };
+
+?>
