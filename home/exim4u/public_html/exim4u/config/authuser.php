b91e8d016cb4044a89f39e0b9f934ea457b4742a
gldickens3 <gordon@mailhub4u.com>
2010-03-04 22:18:34 +0000

    Security patch adminlists.php, authuser.php, authpostmaster.php and authsite.php.

--- home/exim4u/public_html/exim4u/config/authuser.php
+++ home/exim4u/public_html/exim4u/config/authuser.php
@@ -1,16 +1,22 @@
 <?php
   include_once dirname(__FILE__) . "/httpheaders.php";
   include_once dirname(__FILE__) . "/variables.php";
-  $query = "SELECT user_id,localpart,crypt,domain_id FROM users WHERE localpart='".$_SESSION['localpart']."'
-              AND domain_id='".$_SESSION['domain_id']."';";
-  $result = $db->query($query);
-  $row = $result->fetchRow();
 
-  // If the localpart isn't in the cookie, of the database
-  // password doesn't match the cookie password, reject the
-  // user to the login screen
-  if ($row['localpart'] != $_SESSION['localpart']) { header ("Location: index.php?login=failed"); };
-  if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed"); };
-  if ($row['user_id'] != $_SESSION['user_id']) {header ("Location: index.php?login=failed"); };
+  // Test to confirm that the session variables have been set.
+  if ((isset($_SESSION['localpart'])) &&  (isset($_SESSION['crypt'])) &&  (isset($_SESSION['user_id'])) &&  (isset($_SESSION['domain_id'])) )
+       {
+       $query = "SELECT user_id,localpart,crypt,domain_id FROM users WHERE localpart='".$_SESSION['localpart']."'
+                   AND domain_id='".$_SESSION['domain_id']."';";
+       $result = $db->query($query);
+       $row = $result->fetchRow();
 
+       // If the localpart isn't in the cookie, or the database
+       // password doesn't match the cookie password, reject the
+       // user to the login screen
+       if ($row['localpart'] != $_SESSION['localpart']) { header ("Location: index.php?login=failed");  exit(); };
+       if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed");  exit(); };
+       if ($row['user_id'] != $_SESSION['user_id']) {header ("Location: index.php?login=failed");  exit(); };
+       }
+  else
+       { header ("Location: index.php?login=failed"); exit(); }
 ?>
