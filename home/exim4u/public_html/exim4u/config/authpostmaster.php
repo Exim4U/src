4518d9d47a9bf31506b6ac29e69b771a3e7ab63d
gldickens3 <gordon@mailhub4u.com>
2010-03-04 22:18:34 +0000

    Security patch adminlists.php, authuser.php, authpostmaster.php and authsite.php.

--- home/exim4u/public_html/exim4u/config/authpostmaster.php
+++ home/exim4u/public_html/exim4u/config/authpostmaster.php
@@ -2,16 +2,20 @@
   include_once dirname(__FILE__) . "/variables.php";
   include_once dirname(__FILE__) . "/httpheaders.php";
 
-  // Match the crypted password to the database entry
-  // and confirm the user is an admin
-  $query = "SELECT crypt FROM users WHERE localpart='".$_SESSION['localpart']."' and domain_id='".$_SESSION['domain_id']."' AND admin='1';";
-  $results = $db->query($query);
-  $row = $results->fetchRow();
+ // Test to confirm that the session variables have been set.
+  if ((isset($_SESSION['localpart'])) &&  (isset($_SESSION['domain_id'])) && (isset($_SESSION['crypt'])) )
+       {
+       // Match the crypted password to the database entry
+       // and confirm the user is an admin
+       $query = "SELECT crypt FROM users WHERE localpart='".$_SESSION['localpart']."' and domain_id='".$_SESSION['domain_id']."' AND admin='1';";
+       $results = $db->query($query);
+       $row = $results->fetchRow();
 
-  // If the localpart isn't in the cookie, or the database
-  // password doesn't match the cookie password, reject the
-  // user to the login screen
-  // print_r($_SESSION);
-  if (!isset($_SESSION['localpart'])) { header ("Location: index.php?login=failed"); };
-  if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed"); };
+       // If the database password doesn't match the cookie
+       // password, reject the user to the login screen
+       // print_r($_SESSION);
+       if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed"); exit(); };
+       }
+  else
+       { header ("Location: index.php?login=failed"); exit(); }
 ?>
--- home/exim4u/public_html/exim4u/config/authsite.php
+++ home/exim4u/public_html/exim4u/config/authsite.php
@@ -2,15 +2,21 @@
   include_once dirname(__FILE__) . "/httpheaders.php";
   include_once dirname(__FILE__) . "/variables.php";
 
-  // Match the crypted password to the database entry
-  // and confirm the user is a the siteadmin
-  if ($_SESSION['localpart'] != "siteadmin") { header ("Location: index.php?login=failed"); };
-  $query = "SELECT crypt,domain FROM users,domains WHERE localpart='siteadmin' AND domain='admin' AND
-              users.domain_id=domains.domain_id;";
-  $results = $db->query($query);
-  $row = $results->fetchRow();
-
-  // If the cookie password doesn't math the user password
-  // reject them to the login screen
-  if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed"); };
+  // Test to confirm that the session variables have been set.
+  if ((isset($_SESSION['localpart'])) &&  (isset($_SESSION['crypt'])))
+       {
+       // Match the crypted password to the database entry
+       // and confirm the user is a the siteadmin
+       // print_r($_SESSION);
+       if ($_SESSION['localpart'] != "siteadmin")
+            { header ("Location: index.php?login=failed"); exit(); };
+       $query = "SELECT crypt,domain FROM users,domains WHERE localpart='siteadmin' AND domain='admin' AND users.domain_id=domains.domain_id;";
+       $results = $db->query($query);
+       $row = $results->fetchRow();
+       // If the cookie password doesn't match the user password
+       // reject them to the login screen
+       if ($row['crypt'] != $_SESSION['crypt']) { header ("Location: index.php?login=failed"); exit(); };
+       }
+  else
+       { header ("Location: index.php?login=failed"); exit(); }
 ?>
