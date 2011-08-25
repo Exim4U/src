982436bc9f570ffc4f0dfc31389340d5fc179994
gldickens3 <gordon@mailhub4u.com>
2011-08-25 23:39:40 +0000

    Added script for installing exim4u logos into webmail.

---
+++ xtrasw/webmail_logos/install_logos.sh
@@ -0,0 +1,26 @@
+# Script for installing Exim4U logos and favicons in Horde.
+#
+# Copy favicons.
+cp -f ./logos/favicon.ico ../ingo/themes/default/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../kronolith/themes/tango-blue/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../kronolith/themes/silver/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../kronolith/themes/default/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../imp/themes/tango-blue/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../imp/themes/default/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../turba/themes/tango-blue/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../turba/themes/silver/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../turba/themes/default/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../mnemo/themes/tango-blue/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../mnemo/themes/silver/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../mnemo/themes/default/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../nag/themes/tango-blue/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../nag/themes/silver/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../nag/themes/default/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../themes/silver/graphics/favicon.ico 
+cp -f ./logos/favicon.ico ../themes/default/graphics/favicon.ico
+#
+# Copy Login Page Logo
+cp -f ./logos/horde-power1_52px_WebmailGroupware3.png ../themes/default/graphics/horde-power1.png
+#
+# Copy Small Logo .png image.
+cp -f ./logos/favicon.png ../themes/silver/graphics/horde.png
