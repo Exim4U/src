9cd383ebe5f0f8d5c25c3e8a26d17e38cda3249c
gldickens3 <gordon@mailhub4u.com>
2011-08-30 13:13:30 +0000

    Added/Updated webmail scripts for logos, preferences and ownerships.

---
+++ xtrasw/webmail_admin/imp/config/prefs.local.php
@@ -0,0 +1,13 @@
+<?php
+// Should we move messages to a trash folder instead of just marking
+// them as deleted?
+$_prefs['use_trash'] ['value'] = 1;
+// Hide deleted
+$_prefs['delhide'] ['value'] = 1;
+// Disable blocking of html images
+$_prefs['image_replacement'] ['value'] = 0;
+// Make the default compose window be an HTML editor
+$_prefs['compose_html'] ['value'] = 1;
+// Make the default font size for composing email 16 points
+$_prefs['compose_html_font_size'] ['value'] = 16;
+
