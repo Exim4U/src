470570f595cd2ba7858ed0ecae53e6806870dfa2
gldickens3 <gordon@mailhub4u.com>
2011-12-24 19:12:50 +0000

    Fix comments in mysql_expand_crypt.sql.

--- mysql_migrate/mysql_expand_crypt.sql
+++ mysql_migrate/mysql_expand_crypt.sql
@@ -1,9 +1,9 @@
 # Script to widen the crypt field.
 #
-# mysql -u<user_name> -p<password> exim4u < mysql_migrate/mysql_expand_vacation.sql
+# mysql -u<user_name> -p<password> exim4u < mysql_migrate/mysql_expand_crypt.sql
 #     where: <username> = Your MYSQL root username
 #            <password> = Your MYSQL root username's password
 #	     For example, for user_name=MYNAME and password=SECRET then the command would be:
-#	     mysql -uMYNAME -pSECRET exim4u < mysql_migrate/mysql_expand_vacation.sql
+#	     mysql -uMYNAME -pSECRET exim4u < mysql_migrate/mysql_expand_crypt.sql
 #
 alter table users modify crypt VARCHAR(256);
