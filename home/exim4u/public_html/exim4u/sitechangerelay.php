1abea4166e953c9b9e6311e2493c935f3392658b
Exim4U <gordon@mailhub4u.com>
2016-05-25 21:15:34 -0400

    Minor mods to sitechangerelay.php and sitechangerelaysubmit.php. Mostly reformatting.

--- home/exim4u/public_html/exim4u/sitechangerelay.php
+++ home/exim4u/public_html/exim4u/sitechangerelay.php
@@ -1,62 +1,69 @@
 <?php
-  include_once dirname(__FILE__) . "/config/variables.php";
-  include_once dirname(__FILE__) . "/config/authsite.php";
-  include_once dirname(__FILE__) . "/config/functions.php";
-  include_once dirname(__FILE__) . "/config/httpheaders.php";
+include_once dirname(__FILE__) . "/config/variables.php";
+include_once dirname(__FILE__) . "/config/authsite.php";
+include_once dirname(__FILE__) . "/config/functions.php";
+include_once dirname(__FILE__) . "/config/httpheaders.php";
 ?>
 <html>
-  <head>
+<head>
     <title><?php echo _("Exim4U") . ": " . _("Manage Domains"); ?></title>
     <link rel="stylesheet" href="style.css" type="text/css">
-  </head>
-  <body onLoad="document.passwordchange.localpart.focus()">
-    <?php include dirname(__FILE__) . "/config/header.php"; ?>
-    <div id="Menu">
-      <a href="site.php"><?php echo _("Manage Domains"); ?></a><br>
-      <a href="sitepassword.php"><?php echo _("Site Password"); ?></a><br>
-      <br><a href="logout.php"><?php echo _("Logout"); ?></a><br>
-    </div>
-    <div id="Forms">
-      <table align="center">
-
-          <tr><td colspan="2"><h4><?php echo _("Modify Relay Domain Properties"); ?>:</h4>
-
-          <?php 
-          $query = "SELECT * FROM domains WHERE domain_id=:domain_id";
-          $sth = $dbh->prepare($query);
-          $sth->execute(array(':domain_id'=>$_GET['domain_id']));
-          if ($sth->rowCount()) { $row = $sth->fetch(); }
-                echo _("Relay Domain: "); echo $row['domain'];      
-          ?>
-          </td></tr>
-      
-        <td><input name="domain_id" type="hidden" value="<?php print $_GET['domain_id']; ?>">
-            <input name="domain" type="hidden" value="<?php print $_GET['domain']; ?>"></td></tr>
-      
-      <form name="domainchange" method="post" action="sitechangerelaysubmit.php">
-
-      <tr>
-            <td><?php echo _('Relay Server Address'); ?>:</td>
-            <td>
-            <input type="text" size="30" name="relay_address" value="<?php print $row['relay_address']; ?>" class="textfield">
-            </td>
-          </tr>
-            
-        <tr><td><?php echo _("Spamassassin Tag Score"); ?>:</td>
-            <td><input type="text" size="5" name="sa_tag" value="<?php print $row['sa_tag']; ?>" class="textfield"></td></tr>
-        <tr><td><?php echo _("Spamassassin Discard Score"); ?>:</td>
-            <td><input type="text" size="5" name="sa_refuse" value="<?php print $row['sa_refuse']; ?>" class="textfield"></td></tr>
-        <tr><td><?php echo _("Spamassassin"); ?>:</td>
-            <td><input type="checkbox" name="spamassassin" <?php if ($row['spamassassin'] == 1) {print "checked";} ?>></td></tr>
-        <tr><td><?php echo _("Enabled"); ?>:</td>
-            <td><input type="checkbox" name="enabled" <?php if ($row['enabled'] == 1) {print "checked";} ?>></td></tr>
+</head>
+<body onLoad="document.passwordchange.localpart.focus()">
+<?php include dirname(__FILE__) . "/config/header.php"; ?>
+<div id="Menu">
+    <a href="site.php"><?php echo _("Manage Domains"); ?></a><br>
+    <a href="sitepassword.php"><?php echo _("Site Password"); ?></a><br>
+    <br>
+    <a href="logout.php"><?php echo _("Logout"); ?></a><br>
+</div>
+<div id="Forms">
+    <table align="center">
         <tr>
+            <td colspan="2"><h4><?php echo _("Modify Relay Domain Properties"); ?>:</h4>
+                <?php 
+                $query = "SELECT * FROM domains WHERE domain_id=:domain_id";
+                $sth = $dbh->prepare($query);
+                $sth->execute(array(':domain_id'=>$_GET['domain_id']));
+                if ($sth->rowCount()) { $row = $sth->fetch(); }
+                   echo _("Relay Domain: "); echo $row['domain'];      
+                ?>
+            </td>
+        </tr>
         <td><input name="domain_id" type="hidden" value="<?php print $_GET['domain_id']; ?>">
-            <input name="domain" type="hidden" value="<?php print $_GET['domain']; ?>"></td></tr>
-        <tr><td></td><td><input name="submit" size="25" type="submit" value="<?php echo _("Submit Changes"); ?>"></td></tr>
-      </table>
-    </div>
-  </body>
+            <input name="domain" type="hidden" value="<?php print $_GET['domain']; ?>">
+        </td>
+    </tr>
+        <form name="domainchange" method="post" action="sitechangerelaysubmit.php">
+    <tr>
+        <td><?php echo _('Relay Server Address'); ?>:</td>
+        <td><input type="text" size="30" name="relay_address" value="<?php print $row['relay_address']; ?>" class="textfield"></td>
+    </tr>
+    <tr><td><?php echo _("Spamassassin Tag Score"); ?>:</td>
+        <td><input type="text" size="5" name="sa_tag" value="<?php print $row['sa_tag']; ?>" class="textfield"></td></tr>
+    <tr>
+        <td><?php echo _("Spamassassin Discard Score"); ?>:</td>
+        <td><input type="text" size="5" name="sa_refuse" value="<?php print $row['sa_refuse']; ?>" class="textfield"></td></tr>
+    <tr>
+        <td><?php echo _("Spamassassin"); ?>:</td>
+        <td><input type="checkbox" name="spamassassin" <?php if ($row['spamassassin'] == 1) {print "checked";} ?>></td></tr>
+    <tr>
+        <td><?php echo _("Enabled"); ?>:</td>
+        <td><input type="checkbox" name="enabled" <?php if ($row['enabled'] == 1) {print "checked";} ?>></td></tr>
+    <tr>
+        <td><input name="domain_id" type="hidden" value="<?php print $_GET['domain_id']; ?>">
+            <input name="domain" type="hidden" value="<?php print $_GET['domain']; ?>">
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" class="button">
+            <input name="submit" size="25" type="submit" value="<?php echo _("Submit Changes"); ?>">
+        </td>
+    </tr>
+        </form>
+    </table>
+</div>
+</body>
 </html>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
 <!-- This module was added by GLD -->
--- home/exim4u/public_html/exim4u/sitechangerelaysubmit.php
+++ home/exim4u/public_html/exim4u/sitechangerelaysubmit.php
@@ -1,16 +1,25 @@
 <?php
-  include_once dirname(__FILE__) . "/config/variables.php";
-  include_once dirname(__FILE__) . "/config/authsite.php";
-  include_once dirname(__FILE__) . "/config/functions.php";
-  include_once dirname(__FILE__) . "/config/httpheaders.php";
+include_once dirname(__FILE__) . "/config/variables.php";
+include_once dirname(__FILE__) . "/config/authsite.php";
+include_once dirname(__FILE__) . "/config/functions.php";
+include_once dirname(__FILE__) . "/config/httpheaders.php";
 
-  if (isset($_POST['spamassassin'])) {$_POST['spamassassin'] = 1;} else {$_POST['spamassassin'] = 0;}
-  if (isset($_POST['enabled'])) {$_POST['enabled'] = 1;} else {$_POST['enabled'] = 0;}
+if (isset($_POST['spamassassin'])) {
+    $_POST['spamassassin'] = 1;
+} else {
+    $_POST['spamassassin'] = 0;
+}
 
-  if (preg_match("/^\s*$/",$_POST['relay_address'])) {
+if (isset($_POST['enabled'])) {
+    $_POST['enabled'] = 1;
+} else {
+    $_POST['enabled'] = 0;
+}
+
+if (preg_match("/^\s*$/",$_POST['relay_address'])) {
     header("Location: sitechangerelay.php?domain_id={$_POST['domain_id']}&domain={$_POST['domain']}&blankrelayadd=yes");
     die;
-  }
+}
 
 $query = "UPDATE domains SET relay_address=:relay_address,
   	sa_tag=:sa_tag, sa_refuse=:sa_refuse,
@@ -27,14 +36,10 @@ $success = $sth->execute(array(':relay_address'=>$_POST['relay_address'],
 if ($success) {
     header ("Location: site.php?updated={$_POST['domain']}");
     die; 
-  } else {
-    header ("Location: sitechangerelay.php?domain_id={$_POST['domain_id']}&domain={$_POST['domain']}&failupdated={$_POST['domain']}");
-    die;
-  }
+}
   
-
 # Just-in-case catchall
-header ("Location: site.php?failupdated={$_POST['domain']}");
+header ("Location: sitechangerelay.php?domain_id={$_POST['domain_id']}&domain={$_POST['domain']}&failupdated={$_POST['domain']}");
 ?>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
 <!-- This module was added by GLD -->
